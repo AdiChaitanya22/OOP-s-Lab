77f46a78aaae4eed91d25f8ec8c39f5244382878
AdiChaitanya22 <163620750+AdiChaitanya22@users.noreply.github.com>
2025-02-11 11:07:29 +0530

    Add files via upload

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -2,29 +2,17 @@ import java.util.Scanner;
 
 public class SumOfDigits {
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
-
-        
         System.out.print("Enter an integer: ");
         int number = scanner.nextInt();
-
-        
-        number = Math.abs(number);
-
         int sum = 0;
-
-       
+        number = Math.abs(number);
         while (number > 0) {
             int digit = number % 10; 
-            sum += digit;           
-            number /= 10;           
+            sum += digit;        
+            number /= 10;          
         }
-
-      
         System.out.println("The sum of the digits is: " + sum);
-
-       
         scanner.close();
     }
 }
